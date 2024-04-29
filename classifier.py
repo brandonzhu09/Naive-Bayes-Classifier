2134e3e2b0ae4f2bc6e9979407d8dcc7b3d1055c
Shahryar Kiani <shahryarkiani04@gmail.com>
2024-04-29 19:03:39 -0400

    implemented get_bigrams along with some basic datapoints extraction

--- classifier.py
+++ classifier.py
@@ -1,6 +1,5 @@
 import re
-
-
+from collections import defaultdict
 
 log = True
 
@@ -12,12 +11,16 @@ class UserTweet:
 
 
 class User:
-    def __init__(self, user_id):
+    def __init__(self, user_id, label):
         self.user_id = user_id
         self.tweets = []
+        self.label = label
 
     def add_tweet(self, tweet):
         self.tweets.append(tweet)
+
+    def __str__(self) -> str:
+        return str(self.user_id) + " " + str(self.label)
 
 
 def clean_tweet(text):
@@ -32,6 +35,14 @@ def clean_tweet(text):
     text = ' '.join(text.split()) # removing white space
 
     return text
+
+def get_bigrams(text):
+    words = text.split()
+    bigrams = []
+    for i in range(1, len(words)):
+        bigrams.append((words[i - 1], words[i]))
+
+    return bigrams
 
 
 def process_data(file_path):
@@ -57,7 +68,7 @@ def process_data(file_path):
 
                 # Check if user exists in the dict, else create a new User object
                 if user_id not in user_dict:
-                    new_user = User(user_id)
+                    new_user = User(user_id, label)
                     new_user.add_tweet(tweet)
                     users.append(new_user)
                     user_dict[user_id] = new_user
@@ -76,7 +87,34 @@ if log:
     print('processed ' + str(len(users)) + ' users')
 
 
+words = set()
+
+
+bigrams = defaultdict(int)
+
+total_bigrams = 0
+total_words = 0
+
+depressed_count = 0
+
 for user in users:
+    if user.label == "1":
+        depressed_count += 1
     for tweet in user.tweets:
-        pass
-       # print(tweet.text)
+        words.update(tweet.text.split())
+        total_words += len(tweet.text.split())
+        bigram_list = get_bigrams(tweet.text)
+        total_bigrams += len(bigram_list)
+        for big in bigram_list:
+            bigrams[big] = bigrams[big] + 1
+if log:
+    print('processed ' + str(len(words)) + ' unqique words')
+    print('processed ' + str(len(bigrams)) + ' unique bigrams')
+    print('processed ' + str(total_bigrams) + ' total bigrams')
+    print('processed ' + str(total_words) + ' total words')
+    for k, v in list(bigrams.items()):
+        if v <= 15:
+            del bigrams[k]
+    print('processed ' + str(len(bigrams)) + ' frequent bigrams')
+    print(str(depressed_count) + " depressed users")
+    #print(bigrams)
