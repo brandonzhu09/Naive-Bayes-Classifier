080baadce49742dd1a910cbe222670850fc9de84
Shahryar Kiani <shahryarkiani04@gmail.com>
2024-05-01 19:38:54 -0400

    Refactor code to make it easy to run model with various parameters

--- classifier.py
+++ classifier.py
@@ -3,7 +3,8 @@ from collections import defaultdict
 import csv
 import math
 
-log = True
+
+ngram_select = ""
 
 class UserTweet:
     def __init__(self, tweet_id, text, label):
@@ -66,10 +67,17 @@ def get_trigrams(text):
     return trigrams
 
 def get_features(text):
-    return get_bigrams(text)
+    if ngram_select == "unigram":
+        return get_unigrams(text)
+    elif ngram_select == "bigram":
+        return get_bigrams(text)
+    elif ngram_select == "trigram":
+        return get_trigrams(text)
+    else:
+        raise Exception("Need cli argument for n-gram type, choose from [unigram, bigram, trigram]")
 
 
-useless_words = ["i", "you", "and", "my", "it", "is", "that", "im", "the", "or", "of", "for", "zayn", "pillowtalk", "now", "gopaytwin", "yong", "paytforluckysun", "wearepayting", "nowzayn", "bestmusicvideo", "iheartawards", "am", "a", "in", "your"]
+useless_words = ["i", "you", "and", "my", "it", "is", "that", "im", "the", "or", "of", "for", "zayn", "pillowtalk", "now", "gopaytwin", "yong", "paytforluckysun", "wearepayting", "foryong", "nowzayn", "bestmusicvideo", "iheartawards", "am", "a", "in", "your"]
 
 def is_bad_feature(ngram):
     for word in ngram:
@@ -96,8 +104,6 @@ def training_test_split(users, test_size=0.2):
 
     training_depressed_prop = int(0.75 * num_training_data)
     training_normal_prop = num_training_data - training_depressed_prop
-
-    print(training_depressed_prop, training_normal_prop)
 
     training_data = depressed_users[0:training_depressed_prop] + normal_users[0:training_normal_prop]
     test_data = depressed_users[training_depressed_prop:] + normal_users[training_normal_prop:]
@@ -267,10 +273,6 @@ def get_conditional_prob_by_tweet(training_tweets, freq_bigrams):
         else:
             total_normal += 1
 
-
-    print("total depressed tweets read is " + str(total_depressed))
-    print("total normal tweets read is " + str(total_normal))
-
     for feature in depressed_prob:
         depressed_prob[feature] = depressed_prob[feature] / total_depressed
         normal_prob[feature] = normal_prob[feature] / total_normal
@@ -295,8 +297,107 @@ def training_tweet_split(tweets, test_size):
     training_depressed_prop = int(0.5 * num_training_data)
     training_normal_prop = num_training_data - training_depressed_prop
 
-    print(training_depressed_prop, training_normal_prop)
-
     training_data = depressed_tweets[0:training_depressed_prop] + normal_tweets[0:training_normal_prop]
     test_data = depressed_tweets[training_depressed_prop:] + normal_tweets[training_normal_prop:]
     return [training_data, test_data]
+
+def run_classifier(users, training_data, test_data, minfreq, ngram_choice, log, print_features):
+    global ngram_select
+    ngram_select = ngram_choice
+    words = set()
+
+
+    ngrams = defaultdict(int)
+
+    total_ngrams = 0
+    total_words = 0
+
+    depressed_count = 0
+
+    depressed_tweets = 0
+    normal_tweets = 0
+
+
+    for user in users:
+        if user.label == "1":
+            depressed_count += 1
+            depressed_tweets += len(user.tweets)
+        else:
+            normal_tweets += len(user.tweets)
+        for tweet in user.tweets:
+            words.update(tweet.text.split())
+            total_words += len(tweet.text.split())
+            ngram_list = get_features(tweet.text)
+            total_ngrams += len(ngram_list)
+            for ngram in ngram_list:
+                ngrams[ngram] = ngrams[ngram] + 1
+
+    training_tweets = []
+
+    depressed_tweets = 0
+    normal_tweets = 0
+
+    for user in training_data:
+        training_tweets += user.tweets
+        if user.label == "1":
+            depressed_tweets += len(user.tweets)
+        else:
+            normal_tweets += len(user.tweets)
+
+    test_tweets = []
+
+    for user in test_data:
+        test_tweets += user.tweets
+
+    for k, v in list(ngrams.items()):
+        if v <= minfreq:
+            del ngrams[k]
+        elif is_bad_feature(k):
+            del ngrams[k]
+
+    if(print_features):
+        print(ngrams)
+            
+    #depressed_dict, normal_dict = get_conditional_prob(training_data, ngrams)
+
+    depressed_tweet_dict, normal_tweet_dict = get_conditional_prob_by_tweet(training_tweets, ngrams)
+
+
+
+
+
+
+    #prior_for_tweets = (depressed_tweets/(depressed_tweets + normal_tweets))
+
+    prior_for_tweets = get_prior_prob(training_data)[0]
+    correct = 0
+
+    for user in test_data:
+        if classify_feature(user, depressed_tweet_dict, normal_tweet_dict, 0.5, 0.5) == user.label:
+            correct += 1
+        elif log:
+            classification = classify_feature(user, depressed_tweet_dict, normal_tweet_dict, prior_for_tweets, 1 - prior_for_tweets)
+            print('user id ' + str(user.user_id) + ' was classified as ' + str(classification) + ' but was actually ' + str(user.label))
+    print('---------- ngram: ' + ngram_select + ' minfreq: ' + str(minfreq) + '--------------------')
+    print('got ' + str(correct) + ' users correct out of ' + str(len(test_data)))
+
+    correct = 0
+
+    for user in training_data:
+        if classify_feature(user, depressed_tweet_dict, normal_tweet_dict, prior_for_tweets, 1 - prior_for_tweets) == user.label:
+            correct += 1
+        elif log:
+            classification = classify_feature(user, depressed_tweet_dict, normal_tweet_dict, prior_for_tweets, 1 - prior_for_tweets)
+            print('user id ' + str(user.user_id) + ' was classified as ' + str(classification) + ' but was actually ' + str(user.label))
+
+    print('got ' + str(correct) + ' users correct out of ' + str(len(training_data)))
+    print('--------------------------------------------------------------------------\n')
+    if log:
+        print(str(depressed_tweets) + ' total depressed tweets in training set')
+        print(str(normal_tweets) + ' total normal tweets in training set')
+        print('processed ' + str(len(ngrams)) + ' unique ngrams')
+        print('processed ' + str(total_ngrams) + ' total ngrams')
+        print('processed ' + str(total_words) + ' total words')
+        print('processed ' + str(len(ngrams)) + ' frequent ngrams')
+        print(str(depressed_count) + " depressed users")
+        #print(bigrams)
--- main.py
+++ main.py
@@ -1,131 +1,35 @@
 from classifier import *
 
+import classifier
+import sys
+
+#################################################
+
+# TO RUN
+
+# FIRST ARGUMENT IS NGRAM TYPE, CHOOSE FROM [unigram, bigram, trigram]
+
+# SECOND ARGUMENT IS MINIMUM FREQUENCY, INPUT AN INTEGER >= 0
+# THE SECOND ARGUMENT IS USED TO FILTER OUT FEATURES THAT DON'T APPEAR OFTEN ENOUGH IN THE
+# DATASET, IF A FEATURE FREQUENCY <= MINIMUM FREQUENCY, 
+
+#################################################
+
 file_path = 'Mental-Health-Twitter.csv'
+
+log = False
 
 
 users = process_data(file_path)
 
-
-#users = [u for u in users if len(u.tweets) >= 100]
-
 training_data, test_data = training_test_split(users, 0.2)
 
 
-words = set()
+# for i in range(0, 1000, 25):
+#     run_classifier(users, training_data, test_data, i, "unigram", False)
 
-
-bigrams = defaultdict(int)
-
-total_bigrams = 0
-total_words = 0
-
-depressed_count = 0
-
-depressed_tweets = 0
-normal_tweets = 0
-
-tweets = []
-
-for user in users:
-    tweets += user.tweets
-    if user.label == "1":
-        depressed_count += 1
-        depressed_tweets += len(user.tweets)
-    else:
-        normal_tweets += len(user.tweets)
-    for tweet in user.tweets:
-        words.update(tweet.text.split())
-        total_words += len(tweet.text.split())
-        bigram_list = get_features(tweet.text)
-        total_bigrams += len(bigram_list)
-        for big in bigram_list:
-            bigrams[big] = bigrams[big] + 1
-
-training_tweets = []
-
-depressed_tweets = 0
-normal_tweets = 0
-
-for user in training_data:
-    training_tweets += user.tweets
-    if user.label == "1":
-        depressed_tweets += len(user.tweets)
-    else:
-        normal_tweets += len(user.tweets)
-
-test_tweets = []
-
-for user in test_data:
-    test_tweets += user.tweets
-
-for k, v in list(bigrams.items()):
-    if v <= 50:
-        del bigrams[k]
-    elif is_bad_feature(k):
-        del bigrams[k]
-
-#print(bigrams)
-        
-depressed_dict, normal_dict = get_conditional_prob(training_data, bigrams)
-
-depressed_tweet_dict, normal_tweet_dict = get_conditional_prob_by_tweet(training_tweets, bigrams)
-
-#print(depressed_dict)
-print("-------------------------------------------------------------------------------------------")
-#print(normal_dict)
-
-correct = 0
-
-
-prior_for_depressed, prior_for_normal = get_prior_prob(training_data)
-
-
-prior_for_tweets = (depressed_tweets/(depressed_tweets + normal_tweets))
-
-
-find_best_features(depressed_tweet_dict, normal_tweet_dict, prior_for_tweets, 1 - prior_for_tweets)
-
-for user in test_data:
-    if classify_feature(user, depressed_tweet_dict, normal_tweet_dict, prior_for_tweets, 1 - prior_for_tweets) == user.label:
-        correct += 1
-    else:
-        pass
-        #print(user)
-        #print(user.tweets[0].text)
-        #print(classify_feature(user, depressed_dict, normal_dict, prior_for_depressed, prior_for_normal))
-
-print('got ' + str(correct) + ' users correct out of ' + str(len(test_data)))
-
-correct = 0
-
-for user in training_data:
-    if classify_feature(user, depressed_tweet_dict, normal_tweet_dict, prior_for_tweets, 1 - prior_for_tweets) == user.label:
-        correct += 1
-    else:
-        pass
-        #print(user)
-        #print(user.tweets[0].text)
-        #print(classify_feature(user, depressed_dict, normal_dict, prior_for_depressed, prior_for_normal))
-
-print('got ' + str(correct) + ' users correct out of ' + str(len(training_data)))
-
-correct = 0
-
-for tweet in test_tweets:
-    if classify_tweet(tweet, depressed_tweet_dict, normal_tweet_dict, prior_for_tweets, 1 - prior_for_tweets) == tweet.label:
-        correct += 1
-
-print('got ' + str(correct) + ' tweets correct out of ' + str(len(test_tweets)))
-
-
-if log:
-    print('processed ' + str(len(users)) + ' users')
-    print(str(depressed_tweets) + ' total depressed tweets')
-    print(str(normal_tweets) + ' total normal tweets')
-    print('processed ' + str(len(words)) + ' unique words')
-    print('processed ' + str(len(bigrams)) + ' unique bigrams')
-    print('processed ' + str(total_bigrams) + ' total bigrams')
-    print('processed ' + str(total_words) + ' total words')
-    print('processed ' + str(len(bigrams)) + ' frequent bigrams')
-    print(str(depressed_count) + " depressed users")
-    #print(bigrams)
+for i in range(0, 300, 5):
+    run_classifier(users, training_data, test_data, i, "bigram", False, False)
+    
+    
+# run_classifier(users, training_data, test_data, 295, "bigram", False, True)
